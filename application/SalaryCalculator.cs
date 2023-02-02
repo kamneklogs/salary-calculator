c1470bfb1513a53e10c5f9c9d2b895548b478e48
kamneklogs <kamneklogs@gmail.com>
2023-02-01 19:28:26 -0500

    Code cleanup

--- application/SalaryCalculator.cs
+++ application/SalaryCalculator.cs
@@ -1,29 +1,27 @@
 using e05.domain;
 
-namespace e05.application
+namespace e05.application;
+public class SalaryCalculator
 {
-    public class SalaryCalculator
+    public static decimal CalculateSalary(Developer developer)
     {
-        public static decimal CalculateSalary(Developer developer)
+        decimal BaseRule = developer.SalaryByHour * developer.WorkedHours;
+        switch (developer.Type)
         {
-            decimal BaseRule = developer.SalaryByHour * developer.WorkedHours;
-            switch (developer.Type)
-            {
-                case DevType.Junior:
-                    break;
-                case DevType.Intermediate:
-                    BaseRule *= 1.12m; // m is used to indicate that the number is decimal
-                    break;
-                case DevType.Senior:
-                    BaseRule *= 1.25m;
-                    break;
-                case DevType.Lead:
-                    BaseRule *= 1.5m;
-                    break;
-                default:
-                    break;
-            }
-            return BaseRule;
+            case DevType.Junior:
+                break;
+            case DevType.Intermediate:
+                BaseRule *= 1.12m; // m is used to indicate that the number is decimal
+                break;
+            case DevType.Senior:
+                BaseRule *= 1.25m;
+                break;
+            case DevType.Lead:
+                BaseRule *= 1.5m;
+                break;
+            default:
+                break;
         }
+        return BaseRule;
     }
 }
